c4e309bca2d18e740d8f2132cc608b086f6a7490
chrissu777 <christophersu777@gmail.com>
2025-03-04 17:47:09 -0500

    completed basic model logic/migrations

--- backend2/admin_backend/settings.py
+++ backend2/admin_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'videos.apps.VideosConfig',
+    'accounts.apps.AccountsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
