d5f6efaa6d3e270b482013ffff38a2c7777045d5
chrissu777 <christophersu777@gmail.com>
2025-03-04 16:33:37 -0500

    cleaning up old spring stuff

--- adminapi/src/main/java/com/weaponwatch/adminapi/repository/RecordingRepository.java
+++
@@ -1,6 +0,0 @@
-package com.weaponwatch.adminapi.repository;
-import com.weaponwatch.adminapi.model.Recording;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordingRepository extends JpaRepository<Recording, Long> {
-}
