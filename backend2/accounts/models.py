83d1a443be2a984a17d27dbf4d78bb0806269069
chrissu777 <christophersu777@gmail.com>
2025-03-04 20:09:17 -0500

    basic recordings endpoint done

--- backend2/accounts/models.py
+++ backend2/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class School(models.Model):
     name = models.CharField(max_length=255)
+    nces_id = models.CharField(max_length=20,default=0)
 
 class Profile(models.Model):
     ROLE_CHOICES=[
