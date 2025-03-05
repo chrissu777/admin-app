544b669c70e99e515cf1c47551dbffb006866a97
chrissu777 <christophersu777@gmail.com>
2025-03-04 21:34:49 -0500

    basic endpoints

--- backend2/admin_backend/urls.py
+++ backend2/admin_backend/urls.py
@@ -17,14 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+# import videos
+# import accounts
 
-from videos import views
 
-router = routers.DefaultRouter()
-router.register(r'recordings', views.RecordingViewSet)
-
+# router = routers.DefaultRouter()
+# router.register(r'recordings', videos.views.RecordingViewSet)
+# router.register(r'schools', accounts.views.SchoolViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/recordings/', include('videos.urls')),
+    path('api/schools/', include('accounts.urls')),
 ]
