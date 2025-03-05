df172c4e325b763cd4c17749859e49dd8252bf34
chrissu777 <christophersu777@gmail.com>
2025-03-04 23:58:36 -0500

    fixed cors error

--- backend2/admin_backend/settings.py
+++ backend2/admin_backend/settings.py
@@ -39,10 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'videos.apps.VideosConfig',
     'accounts.apps.AccountsConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
-
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:5173',
+)
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
